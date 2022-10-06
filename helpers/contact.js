10b8fd7bea5854a4a3208d2e630af4f026b4d135
daidensacha <daidensacha@gmail.com>
2022-10-06 12:38:22 +0200

    Update get climate zone route

---
+++ helpers/contact.js
@@ -0,0 +1,40 @@
+// /helpers/email.js
+const nodeMailer = require('nodemailer');
+
+const contactMail = async (firstname, lastname, email, message) => {
+  const transporter = await nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: process.env.EMAIL_TO,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+    tls: {
+      ciphers: 'SSLv3',
+    },
+  });
+
+  const mailOption = {
+    from: process.env.EMAIL_TO,
+    to: process.env.EMAIL_TO,
+    subject: 'Contact Form',
+    html: `
+      <h1>Contact Form</h1>
+      <p>First Name: ${firstname}</p>
+      <p>Last Name: ${lastname}</p>
+      <p>Email: ${email}</p>
+      <p>Message: ${message}</p>
+    `,
+  };
+  try {
+    await transporter.sendMail(mailOption);
+    console.log('Message sent');
+    return Promise.resolve('Message sent');
+  } catch (error) {
+    console.log(`Problem sending email: ${error}`);
+    return Promise.reject(error);
+  }
+
+};
--- routes/climate_zone.js
+++ routes/climate_zone.js
@@ -5,5 +5,5 @@ const { get_climate_zone } = require('../controllers/climate_zone');
 
 const { requireSignin } = require('../controllers/auth');
 
-router.get('/climate-zone/:latitude/:longitude', requireSignin, get_climate_zone);
+router.get('/climate-zone/:latitude/:longitude', get_climate_zone);
 module.exports = router;
