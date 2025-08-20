e2d01ae22d6b35930e8a20883e62287d42090d07
daidensacha <daidensacha@gmail.com>
2025-08-20 13:10:42 +0200

    Docs: add coment in utils/valiationRules about movement of valiadation regex.

--- utils/validationRules.js
+++ utils/validationRules.js
@@ -1,3 +1,9 @@
+//  This is no longer used in the backend, replaced by
+// const {
+//   PASSWORD_REGEX,
+//   PASSWORD_MESSAGE,
+// } = require('@daidensacha/almanac-shared');
+
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,20}$/;
 const PASSWORD_MESSAGE =
   'Password must be 8–20 characters and include uppercase, lowercase, and a number.';
